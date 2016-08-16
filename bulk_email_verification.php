b913b6bba9e545dda7b947ffee2321fa0e526b23
nnme <makdak69@gmail.com>
2016-08-16 13:35:44 +0300

    readme changed

--- bulk_email_verification.php
+++ bulk_email_verification.php
@@ -1,3 +1,4 @@
+<!-- upload the file -->
 $key = "PUT YOUR API KEY HERE";
 $settings['file_contents'] ="@/home/Downloads/emails.txt"; //path to your file
 $url = 'https://app.emaillistverify.com/api/verifApiFile?secret='.$key.'&filename=my_emails.txt';
@@ -7,3 +8,10 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $settings);
 $file_id = curl_exec($ch); //you need to save this FILE ID for get file status and download reports in future
 curl_close($ch);
+
+
+<!-- download ready file -->
+$key = "PUT YOUR API KEY HERE";
+            $url = 'https://app.emaillistverify.com/api/getApiFileInfo?secret='.$key.'&id=10700';
+            $string = file_get_contents($url);
+            list($file_id,$filename,$unique,$lines,$lines_processed,$status,$timestamp,$link1,$link2) = explode('|',$string); //parse data
